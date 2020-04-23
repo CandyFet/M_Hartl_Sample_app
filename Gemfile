a4fbf3f2680826d99f68109340d1496d5830c2f5
CandyFet <inorect.a.z@gmail.com>
2020-04-23 16:01:13 +0300

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 gem 'bcrypt', '3.1.13'
 gem 'bootsnap', '1.4.5', require: false
 gem 'bootstrap-sass', '3.4.1'
+gem 'bootstrap-will_paginate', '1.0.0'
+gem 'faker', '2.1.2'
 gem 'jbuilder',   '2.9.1'
 gem 'puma',       '4.3.3'
 gem 'rails',      '6.0.2.2'
@@ -13,6 +15,7 @@ gem 'sass-rails', '5.1.0'
 gem 'sassc-rails', '>= 2.1.0'
 gem 'turbolinks', '5.2.0'
 gem 'webpacker', '4.0.7'
+gem 'will_paginate', '3.1.8'
 
 group :development, :test do
   gem 'byebug',  '11.0.1', platforms: %i[mri mingw x64_mingw]
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,13 +3,29 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  test 'layouts links' do
+  def setup
+    @user = users(:michael)
+  end
+
+  test 'layouts links while logged in' do
+    log_in_as(@user)
     get root_path
     assert_template 'static_pages/home'
     assert_select 'a[href=?]', root_path, count: 2
     assert_select 'a[href=?]', help_path
     assert_select 'a[href=?]', about_path
     assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', users_path
+    assert_select 'a[href=?]', user_path(@user)
+    assert_select 'a[href=?]', edit_user_path(@user)
+  end
+
+  test 'layouts links while logged out' do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', login_path
   end
 
   test 'full tittle helper' do
